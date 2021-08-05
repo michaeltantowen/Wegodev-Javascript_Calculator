2f8664f990a448b9b62c523a44af444f75694224
michaeltantowen <tantowen2002@gmail.com>
2021-08-05 13:44:02 +0700

    add darkmode to the user interface

--- script.js
+++ script.js
@@ -1,25 +1,18 @@
-// Android Calculator
+// Dark Mode Script
+var modeBtn = document.querySelector("#btn");
+var calcu = document.querySelector(".calculator");
+var bg = document.querySelector(".body");
+var screen = document.querySelector("#screen");
+var btn = document.querySelectorAll(".btn-container");
 
-// var screen = document.querySelector("#screen");
-// var btn = document.querySelector(".btn-container");
+modeBtn.addEventListener("click", function () {
+	calcu.classList.toggle("dark-mode");
+	bg.classList.toggle("dark-mode");
+	screen.classList.toggle("dark-mode");
+	btn.classList.toggle("dark-mode");
+});
 
-// btn.addEventListener("click", function(e) {
-//   var btnClicked = e.target;
-//   var btnValue = btnClicked.innerText;
-  
-//   if(btnValue === "C") {
-//     screen.value = "";
-//   } else if(btnValue === "del") {
-//     screen.value = screen.value.slice(0, -1);
-//   } else if(btnValue === "=") {
-//     screen.value = eval(screen.value);
-//   } else {
-//     screen.value = screen.value + btnValue;
-//   }
-
-// });
-
-// IOS Calculator
+// Calculator Function Script
 var screen = document.querySelector("#screen");
 var btn = document.querySelector(".btn-container");
 var resetScreen = false;
@@ -27,34 +20,36 @@ var count = false;
 var tempVal = "";
 var operator = "";
 
-btn.addEventListener("click", function(e) {
-  var btnClicked = e.target;
-  var btnValue = btnClicked.innerText;
-  
-  if(btnValue === "C") {
-    screen.value = "";
-  } else if(btnValue === "del") {
-    screen.value = screen.value.slice(0, -1);
-  } else if(btnValue === "=") {
-    if (count == true) {
+btn.addEventListener("click", function (e) {
+	var btnClicked = e.target;
+	var btnValue = btnClicked.innerText;
+
+	if (btnValue === "C") {
+		screen.value = "";
+	} else if (btnValue === "<") {
+		screen.value = screen.value.slice(0, -1);
+	} else if(btnValue === ".") {
+    screen.value = screen.value + ".";
+  } else if (btnValue === "=") {
+		if (count) {
 			screen.value = eval(tempVal + operator + screen.value);
 			count = false;
 		}
-  } else if(btnClicked.classList.contains('operator')){
-    if(count == true) {
-      screen.value = eval(tempVal + operator + screen.value);
-      count = false;
-    }
-    tempVal = screen.value;
-    operator = btnValue;
-    resetScreen = true;
-  } else {
-    if(resetScreen == true) {
-      screen.value = btnValue;
-      resetScreen = false;
-      count = true;
-    } else {
-      screen.value = screen.value + btnValue;
-    }
-  }
+	} else if (btnClicked.classList.contains("operator")) {
+		if (count) {
+			screen.value = eval(tempVal + operator + screen.value);
+			count = false;
+		}
+		tempVal = screen.value;
+		operator = btnValue;
+		resetScreen = true;
+	} else {
+		if (resetScreen) {
+			screen.value = btnValue;
+			resetScreen = false;
+			count = true;
+		} else {
+			screen.value = screen.value + btnValue;
+		}
+	}
 });
